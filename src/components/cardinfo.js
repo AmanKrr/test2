df9060b6f362a7561fbe21cd37a836a819588b64
Aman Kumar <aman.sinha74458@gmail.com>
2022-06-26 18:22:46 +0530

    update

---
+++ src/components/cardinfo.js
@@ -0,0 +1,12 @@
+// card info for dynamic use
+// import "../img/"
+
+const cardInfo = [
+    {img:"nord", id:"1", cardfname:"Nord Stage 3 88 Keys", price: "$5,738 AUD"},
+    {img:"halix", id:"2", cardfname:"Line6 Helix Rack Guitar Prrocessor - 1x opened box", price: "$1,831.57 AUD"},
+    {img:"nord lead 1", id:"3", cardfname:"Nord Lead A1", price: "$1,700 AUD"},
+    {img:"moog subsequent", id:"4", cardfname:"Moog subsequent 37", price: "$1,700 AUD"},
+    {img:"miniature", id:"5", cardfname:"Moog Minitaur 2010s Black", price: "$682.80 AUD"},
+] 
+
+export default cardInfo;
---
+++ src/components/parent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import cardInfo from './cardinfo'
+import '../css/parent.css'
+
+let strCrd = "card";
+
+function Card(props) {
+    return (
+        <>
+            <div className={strCrd + props.id + " " + "cards"}>
+                <img src={require(`../img/${props.img}.jpeg`)} alt=""></img>
+                <p>{props.cardfname}</p>
+                <p>{props.price}</p>
+            </div>
+        </>
+    )
+}
+
+function createCard(info) {
+    return (
+        <Card id={info.id} key={info.id} img={info.img} cardfname={info.cardfname} price={info.price} />
+    )
+}
+
+function Dashboard() {
+    return (
+        <div className="dashboard-container">
+            <div className="dashboard">
+                {cardInfo.map(createCard)}
+            </div>
+        </div>
+    );
+  }
+  export default Dashboard;
